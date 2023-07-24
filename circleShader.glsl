49e82b2ac90f41b887dfa84da58bcbb8cc881a8b
alxndrpsclt <alxndr.psclt@gmail.com>
2023-07-24 13:35:07 +0200

    Adjusts uv coords to account for aspect ratio

--- circleShader.glsl
+++ circleShader.glsl
@@ -3,16 +3,17 @@ precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 uniform float u_time;
 
 void main() {
-//    vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution) / min(u_resolution.x, u_resolution.y);
-      vec2 uv = gl_FragCoord.xy/u_resolution.xy * 2.0 - 1.0;
+  vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / min(u_resolution.x, u_resolution.y);
+  //vec2 uv = gl_FragCoord.xy/u_resolution.xy * 2.0 - 1.0;
 
-      uv.x *= u_resolution.x / u_resolution.y;
+  //uv.x *= u_resolution.x / u_resolution.y;
 
 
-      float d = length(uv);
+  float d = length(uv);
 //
 //    d -= 0.5;
 //    d = abs(d);
@@ -20,6 +21,6 @@ void main() {
 //    d = smoothstep(0.0, 0.1, d);
 //
 //    gl_FragColor = vec4(vec3(d), 1.0);
-      gl_FragColor = vec4(d, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(d, u_mouse.x/u_resolution.x, u_mouse.y/u_resolution.y, 1.0);
 }
 
