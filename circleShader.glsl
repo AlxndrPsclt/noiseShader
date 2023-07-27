7504fb76ee4b2394634dd38695582927141156fd
alxndrpsclt <alxndr.psclt@gmail.com>
2023-07-27 20:43:07 +0200

    Ajusts color vector

--- circleShader.glsl
+++ circleShader.glsl
@@ -12,14 +12,23 @@ void main() {
 
   //uv.x *= u_resolution.x / u_resolution.y;
 
+  vec2 m = u_mouse.xy/u_resolution.xy;
+  vec3 col = vec3(1.0,0.3,3.0);
+
 
   float d = length(uv);
-  d -= 0.5;
+  float angle = atan(uv.y, uv.x);
+
+  d = sin(d*m.x*20. + 5.*m.y * angle * u_time)/20.;
+
+  //d -= m.x;
   d = abs(d);
-  d = step(0.1, d);
+  //d = step(0.01, d);
+  d = 0.02 / d;
 //d = smoothstep(0.0, 0.1, d);
 
+  col *= d;
   //gl_FragColor = vec4(d, u_mouse.x/u_resolution.x, u_mouse.y/u_resolution.y, 1.0);
-  gl_FragColor = vec4(d, d, d, 1.0);
+  gl_FragColor = vec4(col, 1.0);
 }
 
